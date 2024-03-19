9b239b380f2b580df1d98ac40597ac642e7859c1
Camilla Westin <camilla@webstin.se>
2024-03-19 10:10:24 +0100

    Moved footer to component

---
+++ app/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="bg-mint mt-4 p-2">
+    <div class="container">
+      <div class="flex justify-between items-center">
+        <div>
+          <p class="text-sm font-primary">© 2024 FlavorFusion</p>
+        </div>
+        <div>
+          <a href="#" class="text-sm">Privacy Policy</a>
+          <a href="#" class="text-sm ml-4">Terms of Service</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
