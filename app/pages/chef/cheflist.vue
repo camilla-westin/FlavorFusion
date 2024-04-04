4096fe9e4e0f049f5739737867e9f557094927ae
Camilla Westin <camilla@webstin.se>
2024-04-04 09:45:18 +0200

    Wrapped groq in try catch

--- app/pages/chef/cheflist.vue
+++ app/pages/chef/cheflist.vue
@@ -3,12 +3,23 @@ import { type Chef } from "~/types/Chef";
 import ChefCard from "@/components/ChefCard.vue";
 
 const chefQuery = groq`*[ _type == "chef" && defined(slug.current) ] | order(_createdAt desc)`;
-const { data: chefs } = await useSanityQuery<Chef[]>(chefQuery);
+const isLoading = ref(true);
+let chefs;
+
+try {
+  const result = await useSanityQuery<Chef[]>(chefQuery);
+  chefs = result.data;
+  isLoading.value = false;
+} catch (error) {
+  console.error("Failed to fetch chefs:", error);
+  isLoading.value = false;
+}
 </script>
 <template>
   <section class="p-4 md:p-8">
     <h1 class="font-semibold text-5xl pb-8">Chefs at FlavorFusion</h1>
-    <div class="grid grid-cols-1 2 md:grid-cols-4 gap-6 mt-4">
+    <div v-if="isLoading">Loading...</div>
+    <div v-else class="grid grid-cols-1 2 md:grid-cols-4 gap-6 mt-4">
       <ChefCard v-for="chef in chefs" :key="chef._id" :chef="chef" />
     </div>
   </section>
